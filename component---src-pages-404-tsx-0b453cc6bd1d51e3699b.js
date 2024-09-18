eb22dfd96491ba0e9e1cef6485183bfaef4f7d30
Alejandro Piña <alejjose8@gmail.com>
2024-09-17 21:20:48 -0300

    Updates

---
+++ component---src-pages-404-tsx-0b453cc6bd1d51e3699b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklanding=self.webpackChunklanding||[]).push([[218],{2513:function(e,n,t){t.r(n),t.d(n,{Head:function(){return u}});var l=t(7294);n.default=()=>l.createElement("main",null,l.createElement("h1",null,"Page not found"));const u=()=>l.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-0b453cc6bd1d51e3699b.js.map
